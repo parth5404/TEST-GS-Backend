98880527120c5f20d37c6faa6fa5e992d3b31564
Parth Lahoti <parthlahoti5404@gmail.com>
2025-08-08 17:34:19 +0530

    minor changes

--- go_email/main.go
+++ go_email/main.go
@@ -6,18 +6,25 @@ import (
 	//"html/template"
 	"net/http"
 	"os"
+	"log"
 	"github.com/parth5404/TEST-GS-Backend/go_email/utils"
+	"github.com/joho/godotenv"
 	//"github.com/parth5404/TEST-GS-Backend/go_email/utils"
 )
 
 
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	http.HandleFunc("/send-email", utils.EmailConv)
 
 	port := os.Getenv("PORT")
+	fmt.Println(port)
 	fmt.Println("Server starting on port " + port + "...")
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 	}
